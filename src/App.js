3d9781a374d52dee8a2c8b7a265cdb5684e52ff5
JorgeAjs02 <jajgjorge@gmail.com>
2021-08-02 15:39:27 -0400

    seccion de pedidos casi terminado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Seleccion from './Components/seleccion_pedidos'
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+import { CssBaseline } from '@material-ui/core';
 
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+    secondary: red
+  }
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Seleccion />
+      </CssBaseline>
+    </ThemeProvider>
+
   );
 }
 
