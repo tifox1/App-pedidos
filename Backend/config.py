9fd221db2fbcdeb5ba4fb0c674c49e50378f60a6
imanolarza <imanolarza@gmail.com>
2021-09-04 10:49:51 -0400

    gitignore y db

--- Backend/config.py
+++ Backend/config.py
@@ -5,5 +5,5 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:3142@localhost/A1'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:3142@db/A1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
